56204cd0a9143939b0d6cd2a96551371b0c59476
oskiegarcia <ogarc74@gmail.com>
2018-07-09 23:31:07 +0800

    added initial project codes

---
+++ src/main/java/com/zuhlke/f10apidemo/F10apiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zuhlke.f10apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F10apiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F10apiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zuhlke/f10apidemo/F10apiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zuhlke.f10apidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class F10apiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
