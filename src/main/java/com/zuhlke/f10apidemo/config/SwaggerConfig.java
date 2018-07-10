7720e8349263fce39739422d5134dd460ff27de2
ragzest <raghunath.esturi@gmail.com>
2018-07-10 10:51:30 +0800

    Test Commit

--- src/main/java/com/zuhlke/f10apidemo/config/SwaggerConfig.java
+++ src/main/java/com/zuhlke/f10apidemo/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.zuhlke.f10apidemo;
+package com.zuhlke.f10apidemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
