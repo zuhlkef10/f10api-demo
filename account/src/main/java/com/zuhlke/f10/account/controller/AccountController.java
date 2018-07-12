45594c3aa5b639b5f5b6a88bb008a016111e932a
oskiegarcia <ogarc74@gmail.com>
2018-07-12 10:12:12 +0800

    Account DB Impl

--- account/src/main/java/com/zuhlke/f10/account/controller/AccountController.java
+++ account/src/main/java/com/zuhlke/f10/account/controller/AccountController.java
@@ -1,15 +1,12 @@
 package com.zuhlke.f10.account.controller;
 
 import com.zuhlke.f10.account.exception.UnknownErrorException;
-import com.zuhlke.f10.account.model.Account;
-import com.zuhlke.f10.account.model.AccountRouting;
-import com.zuhlke.f10.account.model.CrAccount;
+import com.zuhlke.f10.account.model.*;
 import com.zuhlke.f10.account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/banks")
@@ -27,25 +24,39 @@ public class AccountController {
 
 
     @RequestMapping(value="/{bankId}/accounts/{accountId}", method = RequestMethod.PUT)
-    public CrAccount createAccount(@PathVariable("bankId") String bankId
+    public AccountCr createAccount(@PathVariable("bankId") String bankId
             , @PathVariable("accountId") String accountId
-            , CrAccount crAccount) {
+            , AccountCr accountCr) {
 
-        Account account = new Account();
-        ArrayList<AccountRouting> accountRoutings = new ArrayList<>();
-        accountRoutings.add(crAccount.getAccountRouting());
-        account.setAccountRoutings(accountRoutings);
-        account.setBalance(crAccount.getBalance());
-        account.setBankId(bankId);
-        account.setId(UUID.randomUUID().toString());
-        account.setLabel(crAccount.getLabel());
+           Account account = new Account();
 
-        Account savedAccount = accountService.create(account);
-        if (savedAccount==null){
+           account.setBankId(bankId);
+           account.setId(accountId);
+
+           account.setNumber(accountCr.getNumber());
+           account.setLabel(accountCr.getLabel());
+           account.setBalance(accountCr.getBalance());
+
+           ArrayList<AccountRouting> accountRoutings  = new ArrayList<>();
+           accountRoutings.add(accountCr.getAccountRouting());
+           account.setAccountRoutings(accountRoutings);
+
+           ArrayList<AccountRule> accountRules = new ArrayList<>();
+           accountRules.add(accountCr.getAccountRule());
+           account.setAccountRules(accountRules);
+
+           ArrayList<Owner> owners = new ArrayList<>();
+           owners.add(accountCr.getOwner());
+           account.setOwners(owners);
+
+           account.setType(accountCr.getType());
+
+          Account savedAccount = accountService.create(account);
+          if (savedAccount==null){
             throw new UnknownErrorException();
-        }
+          }
 
-        return crAccount;
+        return accountCr;
 
 
     }
