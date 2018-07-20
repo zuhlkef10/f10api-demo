7cb296a46e278f486152210b16dc57cff7939c08
oskiegarcia <ogarc74@gmail.com>
2018-07-20 12:59:06 +0800

    Fixed claim insurance.

--- insurance/src/main/java/com/zuhlke/f10/insurance/claims/controller/ClaimsApiController.java
+++ insurance/src/main/java/com/zuhlke/f10/insurance/claims/controller/ClaimsApiController.java
@@ -6,10 +6,7 @@ import com.zuhlke.f10.insurance.model.ClaimDetails;
 import com.zuhlke.f10.insurance.model.ClaimSettlement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,7 +21,8 @@ public class ClaimsApiController implements ClaimsApi {
             produces = {"application/json"},
             consumes = {"application/json"},
             method = RequestMethod.POST)
-    public ResponseEntity<ClaimSettlement> claimInsurance(String policyId, @Valid @RequestBody ClaimDetails body) {
+    public ResponseEntity<ClaimSettlement> claimInsurance(@PathVariable("policyId") String policyId
+                                                         ,@Valid @RequestBody ClaimDetails body) {
 
         return ResponseEntity.ok().body(claimService.claimInsurance(policyId,body));
     }
